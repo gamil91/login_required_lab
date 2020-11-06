6e4d3fe1bfb8d2c5f2d6f1ed2a770b2220afd343
gamil91 <alexis_gamil99@yahoo.com.com>
2020-11-05 21:23:21 -0800

    commit tree d6562d1c8307f3ebe628f626d09e7fee7e252a6b

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # resource :secrets, only:[:show]
+  get '/secret', to: 'secrets#show', as: 'secret'
+  get '/', to: 'application#welcome', as: 'welcome'
+  get '/login', to: 'sessions#new', as: 'login'
+  post '/sessions', to: 'sessions#create', as: 'sessions'
+  delete '/sessions', to: 'sessions#destroy'
+
 end
